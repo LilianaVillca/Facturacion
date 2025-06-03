30bbfadf893cb424f2fe84bde3627270cd52bdec
Liliana Villca <133247782+LilianaVillca@users.noreply.github.com>
2025-06-03 11:42:22 -0300

    Delete Facturacionxd/controlador directory
    delete controller

--- Facturacionxd/controlador/cerrarSession.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("../modelo/conexion.php");
-$usuario = new Conexion();
-$usuario->cerrarSesion();
-// Redirigir al usuario a la página de inicio
-header("Location: ../index.php");
-exit();
-?>
-
